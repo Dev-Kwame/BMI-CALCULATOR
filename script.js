c080c32066dfb49811fd0013be81ec70e629b68c
Dev-Kwame <kwame.agyabeng@amalitech.org>
2023-09-27 00:20:42 +0800

    Initial files

--- script.js
+++ script.js
@@ -1,27 +1,79 @@
-const metricButton = document.getElementById('metric');
-const imperialButton = document.getElementById('imperial');
-const metricContent = document.querySelector('.measurement-metric');
-const imperialContent = document.querySelector('.measurement-imperial');
-const rightLine = document.querySelector('.right-line');
+var metricButton = document.getElementById('metric');
+var imperialButton = document.getElementById('imperial');
+var metricContent = document.querySelector('.measurement-metric');
+var imperialContent = document.querySelector('.measurement-imperial');
+var rightLine = document.querySelector('.right-line');
+var heightCMInput = document.getElementById('heightCM');
+var weightKGInput = document.getElementById('weightKG');
+var heightFTInput = document.getElementById('heightFT');
+var heightINInput = document.getElementById('heightIN');
+var weightSTInput = document.getElementById('weightST');
+var weightLBSInput = document.getElementById('weightLBS');
+var welcomeMessage = document.querySelector('.welcome');
+var welcomeInput = document.getElementById('open');
+var result = document.querySelector('.result');
+var resultValue = document.querySelector('.result-value');
+var finalResult = document.querySelector('.final_results');
 // Function to show the metric content and hide the imperial content
 function showMetricContent() {
-  
+    if (metricButton)
         metricButton.checked = true; // Ensure Metric is checked
+    if (imperialButton)
         imperialButton.checked = false; // Uncheck Imperial
+    if (metricContent)
         metricContent.classList.remove("hidden");
+    if (imperialContent)
         imperialContent.classList.add("hidden");
 }
 // Function to show the imperial content and hide the metric content
 function showImperialContent() {
-   
+    if (imperialButton)
         imperialButton.checked = true;
+    if (metricButton)
         metricButton.checked = false;
+    if (imperialContent)
         imperialContent.classList.remove("hidden");
+    if (metricContent)
         metricContent.classList.add("hidden");
+    if (rightLine)
         rightLine.style.display = "none";
 }
 // Add event listeners to the radio buttons
+if (metricButton)
     metricButton.addEventListener("click", showMetricContent);
+if (imperialButton)
     imperialButton.addEventListener("click", showImperialContent);
 // Initially, show the metric content (since it's the default)
 showMetricContent();
+function calculateBMI() {
+    // Get the height in centimeters and weight in kilograms from input fields
+    var heightInCM = parseFloat(heightCMInput.value);
+    var weightInKG = parseFloat(weightKGInput.value);
+    // Check if height and weight are valid numbers
+    if (isNaN(heightInCM) || isNaN(weightInKG)) {
+        // Display the "welcome" message if either value is empty
+        if (welcomeMessage) {
+            welcomeMessage.style.display = 'block';
+            welcomeInput.style.display = 'block';
+            result.style.display = 'none';
+        }
+        return;
+    }
+    //Calculate BMI using the metric units
+    var metricBMI = weightInKG / (Math.pow((heightInCM / 100), 2));
+    //hide welcome message and display results
+    if (welcomeMessage)
+        welcomeMessage.style.display = 'none';
+    if (welcomeInput)
+        welcomeInput.style.display = 'none';
+    if (result)
+        result.style.display = 'flex';
+    // Display the BMI result in the "result" div
+    if (finalResult)
+        finalResult.innerText = metricBMI.toFixed(1);
+}
+// Add input event listeners to trigger BMI calculation
+if (heightCMInput)
+    heightCMInput.addEventListener("input", calculateBMI);
+if (weightKGInput)
+    weightKGInput.addEventListener("input", calculateBMI);
--- script.ts
+++ script.ts
@@ -1,8 +1,20 @@
-const metricButton: HTMLInputElement |null = document.getElementById('metric') as HTMLInputElement
-const imperialButton: HTMLInputElement | null = document.getElementById('imperial') as HTMLInputElement
+const metricButton: HTMLInputElement = document.getElementById('metric') as HTMLInputElement;
+const imperialButton: HTMLInputElement  = document.getElementById('imperial') as HTMLInputElement;
 const metricContent: Element | null = document.querySelector('.measurement-metric')
 const imperialContent:  Element | null = document.querySelector('.measurement-imperial')
 const rightLine: HTMLElement | null = document.querySelector('.right-line')
+const heightCMInput: HTMLInputElement| null = document.getElementById('heightCM') as HTMLInputElement;
+const weightKGInput: HTMLInputElement | null = document.getElementById('weightKG') as HTMLInputElement;
+const heightFTInput: HTMLInputElement = document.getElementById('heightFT') as HTMLInputElement;
+const heightINInput: HTMLInputElement = document.getElementById('heightIN') as HTMLInputElement;
+const weightSTInput: HTMLInputElement = document.getElementById('weightST') as HTMLInputElement;
+const weightLBSInput: HTMLInputElement = document.getElementById('weightLBS') as HTMLInputElement;
+const welcomeMessage: HTMLElement = document.querySelector('.welcome');
+const welcomeInput: HTMLElement = document.getElementById('open')
+const result: HTMLElement = document.querySelector('.result')
+const resultValue: HTMLElement | null = document.querySelector('.result-value');
+const finalResult: HTMLElement | null = document.querySelector('.final_results')
+
 
 
 
@@ -13,9 +25,7 @@ function showMetricContent(): void {
     if(metricButton) metricButton.checked = true; // Ensure Metric is checked
     if(imperialButton) imperialButton.checked = false; // Uncheck Imperial
     if(metricContent) metricContent.classList.remove("hidden");
-    if(imperialContent) imperialContent.classList.add("hidden");
-     
-    
+    if(imperialContent) imperialContent.classList.add("hidden"); 
   
   }
 
@@ -26,15 +36,56 @@ function showMetricContent(): void {
     if(imperialContent)imperialContent.classList.remove("hidden");
     if(metricContent)metricContent.classList.add("hidden");
     if(rightLine)rightLine.style.display = "none";
-    
-  }
-
-   
-    
+  
+  } 
   
    // Add event listeners to the radio buttons
-   if(metricButton) metricButton.addEventListener("click", showMetricContent);
-   if(imperialButton) imperialButton.addEventListener("click", showImperialContent);
+   if(metricButton)metricButton.addEventListener("click", showMetricContent)
+   if(imperialButton)imperialButton.addEventListener("click", showImperialContent)
+    
 
  // Initially, show the metric content (since it's the default)
  showMetricContent();
+ 
+
+ function calculateBMI(): void {
+   // Get the height in centimeters and weight in kilograms from input fields
+   const heightInCM: number = parseFloat(heightCMInput.value);
+   const weightInKG: number = parseFloat(weightKGInput.value);
+
+    // Check if height and weight are valid numbers
+    if (isNaN(heightInCM) || isNaN(weightInKG)) {
+
+      // Display the "welcome" message if either value is empty
+      if (welcomeMessage) {
+        welcomeMessage.style.display = 'block';
+        welcomeInput.style.display = 'block';
+        result.style.display = 'none';
+    }
+      return;
+ }
+
+ //Calculate BMI using the metric units
+ const metricBMI: number =  weightInKG / ((heightInCM / 100) ** 2);
+
+ 
+
+ //hide welcome message and display results
+ if(welcomeMessage)welcomeMessage.style.display = 'none';
+ if(welcomeInput)welcomeInput.style.display = 'none';
+ if(result)result.style.display = 'flex';
+
+ // Display the BMI result in the "result" div
+  if(finalResult)finalResult.innerText = metricBMI.toFixed(1);
+  
+
+ 
+ }
+
+
+ // Add input event listeners to trigger BMI calculation
+if (heightCMInput) heightCMInput.addEventListener("input", calculateBMI);
+if (weightKGInput) weightKGInput.addEventListener("input", calculateBMI);
+
+
+ 
